dcf2927401b2b94b625e5440c7ef64d76834651c
kschat <kyleschattler@gmail.com>
2013-02-15 19:10:18 -0500

    setup open-closed-principle for routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
+var fs = require('fs');
 
 /*
- * GET home page.
+ * Static page route module
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Scheduler' });
-};
+exports.init = function init(app) {
+	app.get('/', function(req, res){
+	  res.render('index', { title: 'Scheduler' });
+	});
+
+	app.get(/\/(about|login)/, function(req, res, next) {
+		console.log(req.params);
+	  if(fs.existsSync(app.get('views') + '/' + req.params[0] + '.ejs')) {
+	    res.render(app.get('views') + '/' + req.params[0]);
+	  }
+	  else {
+	    res.render('error', { error: 'Page not found'});
+	  }
+	});
+}
