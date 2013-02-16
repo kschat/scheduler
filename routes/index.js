c42e331218b6bcbe69cf6530f9001a50702e80db
kschat <kyleschattler@gmail.com>
2013-02-15 20:02:45 -0500

    Added log in and sign up pages and modals

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ var fs = require('fs');
  */
 
 exports.init = function init(app) {
+	//Index page route
 	app.get('/', function(req, res){
 	  res.render('index', { title: 'Scheduler' });
 	});
 
+	//Route for any other static page
 	app.get(/\/(about|login)/, function(req, res, next) {
 		console.log(req.params);
 	  if(fs.existsSync(app.get('views') + '/' + req.params[0] + '.ejs')) {
