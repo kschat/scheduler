5e456a38fade3d95b3198f2d8708ad246d1e36d3
kschat <kyleschattler@gmail.com>
2013-03-02 21:29:57 -0500

    Finished implementing the REST GET request porition of the API. Also added a default config file for models lacking one.

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,19 @@
 var fs = require('fs'),
 	path = require('path');
 
+function checkAllowed(request, allowed) {
+	for(var i=0; i<allowed.length; i++) {
+		if(request === allowed[i]) { return true; }
+	}
+
+	return false;
+}
+
 function runOptions(model, sParams, query, callBack) {
-	callBack = callBack || function(err, options) { 
+	var callBack = callBack || function(err, options) { 
 		if(err) {
 			throw err; 
 		}
-
-		console.log(options);
 	},
 	list = [];
 
@@ -25,75 +31,93 @@ function runOptions(model, sParams, query, callBack) {
 	callBack(false, list);
 }
 
-function RESTGet(req, res) {
-	var Model = {},
-		modelSettings = {},
-		parameters = {},
-		payload = {};
+function filterOptions(options, model) {
+	var query = model.find() || {};
 
+	for(var i=0; i<options.length; i++) {
+		var option = options[i];
+		for(property in option) {
+			if(!option.hasOwnProperty(property)) { continue; }
+
+			if(property === 'limit') { query[property](option[property]); }
+			else {
+				query.where(property).equals(option[property]);
+			}
+		}
+	}
+
+	return query;
+}
+
+function loadModel(model) {
+	var Model = {},
+		modelSettings = {};
 	//Get the model and it's settings based on the first API parameter
 	//Handle any errors that may occur
 	try {
-		Model = require('../models/' + req.params[0]);
-		modelSettings = JSON.parse(fs.readFileSync(path.join(__dirname, '../models/config/'+ req.params[0] + '.json')));
+		Model = require('../models/' + model);
+		modelSettings = JSON.parse(fs.readFileSync(path.join(__dirname, '../models/config/'+ model + '.json')));
 	}
 	catch(ex) {
-		console.log(ex);
 		//The client requested a model that doesn't exist.
 		if(ex.code === 'MODULE_NOT_FOUND') {
-			res.send(404, req.params[0] + ' does not exist.');
-			return req.params[0] + ' does not exist.';
+			return { error: true, code: 404, message: model + ' does not exist.' };
 		}
-		//A config file for the model doesn't exist.
+		//A config file for the model doesn't exist, load the default one.
 		else if(ex.code === 'ENOENT') {
-			modelSettings = {
-				methods: [],
-				params: {
-					optional: []
-				},
-			};
+			modelSettings = JSON.parse(fs.readFileSync(path.join(__dirname, '../models/config/default.json')));
 		}
 		else {
-			res.send(500, 'Error: ' + ex);
-			return;
+			return { error: true, code: 500, message: 'Error: ' + ex };
 		}
 	}
 
-	if(req.params[2] === true) {
-		Model.find(req.params[2], function(err, users) {
+	return { error: false, model: Model, modelSettings: modelSettings };
+}
+
+function RESTGet(req, res) {
+	var Model = loadModel(req.params[0]);
+
+	if(Model.error === true) {
+		res.send(Model.code, Model.message);
+		return;
+	}
+
+	var modelSettings = Model.modelSettings;
+	Model = Model.model;
+
+	if(!checkAllowed('GET', modelSettings.methods)) {
+		res.send(405, 'Allow: ' + modelSettings.methods);
+		return;
+	}
+
+	if(typeof req.params[2] != 'undefined') {
+		Model.find({ _id: req.params[2] }, function(err, users) {
 			if(err) { 
 				res.send(503, { error: 'Error contacting database:' });
 				return 'Error contacting database:';
 			}
 
 			res.send(users);
+			return;
 		});
 	}
 	else {
 		runOptions(Model, modelSettings.params, req.query, function(err, options) {
+			var query = filterOptions(options, Model);
 			if(err) {
-				res.send(err); 
-				return;
+				res.send(err.code, err.message);
 			}
-			var query = Model.find();
-
-			for(var i=0; i<options.length; i++) {
-				var option = options[i];
-				for(property in option) {
-					if(!option.hasOwnProperty(property)) { continue; }
-
-					if(property === 'limit') { query[property](option[property]); }
-					else {
-						query.where(property).equals(option[property]);
-					}
-				}
+			else {
+				query.exec(function(err, results) {
+					res.send(results);
+				});
 			}
-
-			query.exec(function(err, results) {
-				res.send(results);
-			});
 		});
 	}
+}
+
+function RESTPost(req, res) {
 
 }
 
@@ -104,9 +128,7 @@ exports.init = function init(app) {
 	* Matches: 
 	* /api/<controller> where the controller is required
 	*/
-	app.post(/^\/api\/([a-zA-Z]+)\/?$/, function(req, res) {
-		res.send(req.body.test);
-	});
+	app.post(/^\/api\/([a-zA-Z]+)\/?$/, RESTPost);
 
 	/*
 	* Generic PUT REST implementation.
@@ -137,5 +159,3 @@ exports.init = function init(app) {
 		res.send(req.params);
 	});
 }
-
-exports.get = RESTGet;
