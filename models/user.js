a1b9394d49febe149616ebe08d85113bb34934f7
kschat <kyleschattler@gmail.com>
2013-04-17 13:04:24 -0400

    Profile image uploading is functional.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
-	bcrypt = require('bcrypt');
+	bcrypt = require('bcrypt'),
+	fs = require('fs');
 
 function validateName(val) {
 	return val.length >= 2 && val.length <= 35;
@@ -35,32 +36,43 @@ function validatePasswordWeak(val) {
 	return validatePassword(val) && val.length < 8;
 }
 
+var PictureSchema = mongoose.Schema({
+	data: {
+		type: Buffer,
+		required: true
+	},
+	filename: {
+		type: String,
+		required: true
+	}
+});
+
 var userSchema = mongoose.Schema({
 	firstName: { 
 		type: String, 
 		required: true,
 		validate: [
-			validateName, 'Error with first name',
-		],
+			validateName, 'Error with first name'
+		]
 	},
 	lastName: {
 		type: String, 
 		required: true,
 		validate: [
-			validateName, 'Error with last name',
-		],
+			validateName, 'Error with last name'
+		]
 	},
 	userName: {
 		type: String, 
 		required: true,
 		index: {
-			unique: true,
-		},
+			unique: true
+		}
 	},
 	description: {
 		type: String,
 		required: false,
-		default: 'No description',
+		default: 'No description'
 	},
 	email: {
 		type: String, 
@@ -69,19 +81,18 @@ var userSchema = mongoose.Schema({
 			unique: true,
 		},
 		validate: [
-			validateEmail, 'Error with email address',
-		],
+			validateEmail, 'Error with email address'
+		]
 	},
 	password: {
 		type: String, 
 		required: true,
 		validate: [
-			{ validator: validatePassword, msg: 'Password too short' },
-		],
+			{ validator: validatePassword, msg: 'Password too short' }
+		]
 	},
 	avatar: {
-		type: String,
-		default: '/img/defaultProfile.jpg'
+		type: [PictureSchema]
 	}
 },
 {
@@ -91,6 +102,7 @@ var userSchema = mongoose.Schema({
 userSchema.pre('save', function(next) {
 	var user = this;
 
+	//Password pre-save
 	if(!user.isModified('password')) { return next(); }
 
 	bcrypt.genSalt(10, function(err, salt) {
@@ -98,6 +110,50 @@ userSchema.pre('save', function(next) {
 			if(err) { return next(err); }
 
 			user.password = hash;
+			next();
+		});
+	});
+});
+
+userSchema.pre('save', function(next) {
+	var user = this;
+
+	if(!user.isModified('avatar')) { return next(); }
+
+	var imageName = user.avatar[0].filename,
+		imageBuffer = user.avatar[0].data,
+		uploadPath = 'public/img/uploads/' + user.userName,
+		dataRegex = /^data:.+\/(.+);(.+),(.*)$/,
+		matches = imageBuffer.toString().match(dataRegex);
+	
+	//If there are no matches then the file isn't a new upload.
+	if(!matches) { return next(); }
+
+	var ext = matches[1],
+		encoding = matches[2],
+		data = matches[3];
+
+	//Converts jpeg to jpg for consistancy
+	ext = ext === 'jpeg' ? 'jpg' : ext;
+
+	//Takes off the file extension in favor of the ext sent with the data url
+	imageName = imageName.substring(0, imageName.lastIndexOf('.'));
+
+	//Creates a buffer that only contains the image data
+	data = new Buffer(data, encoding);
+
+	//Checks if the user already has a file containing their uploads, creates one if they don't.
+	//Writes the upload image to their upload directory
+	fs.exists(uploadPath, function(exists) {
+		if(!exists) { 
+			fs.mkdirSync(uploadPath);
+		}
+
+		fs.writeFile(uploadPath + '/' + imageName + '.' + ext, data, function(err) {
+			if(err) { return next(err); }
+
+			//Resave the the image name to the unique name
+			user.avatar[0].filename = imageName + '.' + ext;
 			next();
 		});
 	});
--- public/js/views/user/profileHeader.js
+++ public/js/views/user/profileHeader.js
@@ -13,20 +13,27 @@ define([
 			this.model = options.model;
 			this.editBtn = new EditButtonView();
 			this.$errorMessage = this.$el.find('#message-container');
+			this.previewImage = {};
 
 			_.bindAll(this, 'render', 'editProfile', 'updateModel', 'updateSuccess', 'updateError');
 		},
 		render: function() {
+			console.log(this.model.attributes);
+			
+			//Temp hack to sync the server and client models since mongoose doesn't allow for a single embedded schema
+			//if(this.model.attributes.avatar[0]) { this.model.set('avatar', this.model.get('avatar')[0]);}
+
 			this.$el.html(this.template(this.model.attributes));
 			this.editBtn.setElement(this.$el.find('#edit-btn'));
 			this.$errorMessage = this.$el.find('#message-container');
-			$('#image-upload-form').ajaxForm();
+			//$('#image-upload-form').ajaxForm();
 			
 			return this;
 		},
 		events: {
 			'click #edit-btn': 			'editProfile',
-			'click #image-upload-btn': 	'uploadImage'
+			'click #image-upload-btn': 	'uploadImage',
+			'change #image-file': 		'readFile'
 		},
 		el: '.profile-header',
 		template: _.template(Template),
@@ -45,8 +52,38 @@ define([
 				this.render();
 			}
 		},
-		uploadImage: function() {
-			$('#image-upload-form').ajaxSubmit();
+		readFile: function(e) {
+			var file = e.target.files[0], 
+				reader = new FileReader();
+
+			if(!file.type.match('image.*')) {
+				console.log('not an image');
+				return false;
+			}
+
+			reader.onload = (function(theFile, self) {
+				return function(e) {
+					var imgObj = {
+						avatar: [{ 
+							filename: self.generateUniqueName(theFile.name),
+							data: e.target.result
+						}]
+					};
+
+					self.previewImage = imgObj;
+					self.$el.find('#preview').attr('src', e.target.result);
+					self.$el.find('#image-upload-btn').removeAttr('disabled');
+				};
+			})(file, this);
+
+			reader.readAsDataURL(file);
+
+			return true;
+		},
+		uploadImage: function(e) {
+			this.model.set(this.previewImage, {silent: true});
+			//Update the current picture on the users profile
+			this.$el.find('#profile-image').attr('src', this.model.attributes.avatar[0].data);
 		},
 		updateModel: function($element) {
 			var obj = "{",
@@ -64,6 +101,9 @@ define([
 			objInst = JSON.parse(obj);
 			
 			this.model.set(objInst);
+		},
+		generateUniqueName: function(fileName) {
+			return new Date().getTime() + '-' + fileName;
 		},
 		updateSuccess: function() {
 			this.template = _.template(Template);
