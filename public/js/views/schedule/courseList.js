9ff6f140ce0c12ac2b369fb1fe24ae07f42cb361
kschat <kyleschattler@gmail.com>
2013-04-25 17:13:49 -0400

    Refactored the views so render builds the view rather than a be a do everything function.

--- public/js/views/schedule/courseList.js
+++ public/js/views/schedule/courseList.js
@@ -3,33 +3,37 @@ define([
 	'underscore',
 	'backbone',
 	'views/schedule/courseSearchItem',
-	'views/loader/loading'
-], function($, _, Backbone, CourseSearchItem, LoadingOverlay) {
+	'views/loader/loading',
+	'text!templates/schedule/courseList.html'
+], function($, _, Backbone, CourseSearchItem, LoadingOverlay, Template) {
 	var CourseListView = Backbone.View.extend({
 		initialize: function(options) {
-			this.$listContainer = this.$el;
-			this.$list = this.$el.find('#course-list');
-			this.$message = this.$el.find('#course-message');
-			this.loading = new LoadingOverlay({ dispatcher: options.dispatcher });
-
-			_.bindAll(this, 'render', 'searchError', 'searchSuccess');
+			_.bindAll(this, 'render', 'resetList', 'searchError', 'searchSuccess');
 			this.dispatcher = options.dispatcher;
 
 			//Events called by the search view notifying this view of its state
 			this.dispatcher.on('search:error', this.searchError);
 			this.dispatcher.on('search:success', this.searchSuccess);
 
-			this.collection.on('add', this.render, this);
-			this.collection.on('reset', this.render, this);
+			this.collection.on('add', this.resetList, this);
+			this.collection.on('reset', this.resetList, this);
 		},
 		el: '#course-container',
+		template: _.template(Template),
 		render: function() {
+			this.$el.html(this.template());
+			this.$list = this.$el.find('#course-list');
+			this.$message = this.$el.find('#course-message');
+			this.loading = new LoadingOverlay({ dispatcher: this.dispatcher });
+
+			return this;
+		},
+		resetList: function() {
 			if(this.collection.length > 0) {
 				this.$list.html('');
 				this.$message.hide();
 				var self = this;
 				this.collection.each(function(course) {
-
 					var cv = new CourseSearchItem({ model: course });
 					self.$list.append(cv.render().el);
 				});
