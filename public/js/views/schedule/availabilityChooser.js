21215144422e894abf7a95689d939d6acc386207
kschat <kyleschattler@gmail.com>
2013-04-26 16:13:46 -0400

    Fixed various bugs with selecting over the ranges and converting from am to noon.

--- public/js/views/schedule/availabilityChooser.js
+++ public/js/views/schedule/availabilityChooser.js
@@ -14,7 +14,7 @@ define([
 			this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 			_.bindAll(this, 'render', 'handleVisibility', 'setUnavailable', 'cacheTime', 'validateTimes','convertToMilitary', 'parseTime',
-				'disableSave', 'enableSave', 'detectChange', 'getRange');
+				'disableSave', 'enableSave', 'detectChange', 'getRange', 'clearTemps');
 			this.dispatcher.on('pager:btnClicked', this.handleVisibility);
 		},
 		el: '#availability-view',
@@ -52,10 +52,11 @@ define([
 
 							minutes = minutes.substring(0, minutes.length-2);
 							console.log(minutes);
-							if(hour === 12) {
-								hour = 0;
+							if(hour === 11) {
 								ampm = ampm.toLowerCase() === 'am' ? 'pm' : 'am';
 							}
+
+							hour = hour === 12 ? 0 : hour;
 							temp = (hour + 1) + ':00' + ampm;
 
 							return template({ 
@@ -152,17 +153,36 @@ define([
 		validateTimes: function(e) {
 			if(!this.timeValidation.test($(e.target).val().toLowerCase())) {
 				$(e.target).val(this.cachedTime).focus();
-				this.$el.find('#save-btn').attr('disabled', 'disabled');
+				this.disableSave();
 				return false;
 			}
 
 			var sTime = this.$el.find('#start-time').val(),
 				eTime = this.$el.find('#end-time').val();
 
-			//Safe guard against users input a start time that begins before the end time
+			//Safe guard against users inputing a start time that begins after the end time
 			if(this.convertToMilitary(sTime) > this.convertToMilitary(eTime)) {
 				$(e.target).focus();
-				return false
+				this.disableSave();
+				return false;
+			}
+
+			//Checks against max values for start and end time
+			if(parseInt(this.convertToMilitary(eTime)) > 2300) {
+				$(e.target).val('11:00pm');
+			}
+
+			if(parseInt(this.convertToMilitary(sTime)) > 2230) {
+				$(e.target).val('10:30pm');
+			}
+
+			//Checks against min values for start and end time
+			if(parseInt(this.convertToMilitary(eTime)) < 630) {
+				$(e.target).val('6:30am');
+			}
+
+			if(parseInt(this.convertToMilitary(sTime)) < 600) {
+				$(e.target).val('6:00am');
 			}
 
 			//Updates the cached value once everything is validated
@@ -173,12 +193,14 @@ define([
 				sday = this.$selectedTime.data('day'),
 				times = this.getRange(sTime, eTime);
 
+			this.clearTemps();
 			this.setRangeState(times, sday, 'selected-time-temp');
 			this.enableSave();
 			return true;
 		},
 		setUnavailable: function(e) {
 			e.preventDefault();
+
 			var sTime = this.$el.find('#start-time').val(),
 				eTime = this.$el.find('#end-time').val(),
 				sday = this.$selectedTime.data('day'),
@@ -202,17 +224,21 @@ define([
 					var hour = parseInt(curr.split(':')[0], 10),
 						ampm = curr.substring(curr.length - 2);
 					
-					if(hour === 12) {
-						hour = 0;
+					if(hour === 11) {
 						ampm = ampm.toLowerCase() === 'am' ? 'pm' : 'am';
 					}
+					hour = hour === 12 ? 0 : hour;
 					temp = (hour + 1) + ':00' + ampm;
+					console.log(temp);
 				}
 				range.push(temp);
 				curr = temp;
 			}
 			range.pop();
 			return range;
+		},
+		clearTemps: function() {
+			this.$el.find('.selected-time-temp').removeClass('selected-time-temp');
 		},
 		disableSave: function() {
 			this.$el.find('#save-btn').attr('disabled', 'disabled');
@@ -225,9 +251,6 @@ define([
 
 			if(this.cachedTime !== content) {
 				this.disableSave();
-			}
-			else {
-				this.enableSave();
 			}
 		}
 	});
